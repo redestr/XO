88ac011d189dfa7628eda5b795161422bafdd8df
redestr <55774228+redestr@users.noreply.github.com>
2022-02-10 17:54:16 +0700

    Ошибочная загрузка файла

--- main.py
+++ main.py
@@ -1,16 +1,83 @@
-# This is a sample Python script.
+def hello():
+    print("-------------------")
+    print("  Приветсвуем вас  ")
+    print("      в игре       ")
+    print("  крестики-нолики  ")
+    print("-------------------")
+    print(" формат ввода: x y ")
+    print(" x - номер строки  ")
+    print(" y - номер столбца ")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def show_table():
+    print()
+    print("    0 1 2")
+    for i, row in enumerate(table):
+        row_str = f"  {i} {' '.join(row)}"
+        print(row_str)
+    print()
 
+def ask_coordinate():
+    while True:
+        cords = input("Ваш ход: ").split()
+        
+        if len(cords) != 2:
+            print(" Введите 2 координаты! ")
+            continue
+        
+        x, y = cords
+        
+        if not(x.isdigit()) or not(y.isdigit()):
+            print(" Введите числа! ")
+            continue
+        
+        x, y = int(x), int(y)
+        
+        if 0 > x or x > 2 or  0 > y or  y > 2 :
+            print(" Координаты вне диапазона! ")
+            continue
+        
+        if table[x][y] != " ":
+            print(" Клетка занята! ")
+            continue
+        
+        return x, y
+            
+def check_win():
+    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+    for cord in win_cord:
+        symbols = []
+        for c in cord:
+            symbols.append(table[c[0]][c[1]])
+        if symbols == ["X", "X", "X"]:
+            print("Выиграл X")
+            return True
+        if symbols == ["0", "0", "0"]:
+            print("Выиграл 0")
+            return True
+    return False
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+hello()
+table = [[" "] * 3 for i in range(3)]
+count = 0
+while count < 9:
+    count += 1
+    show_table()
+    if count % 2 == 1:
+        print(" Ходит X")
+    else:
+        print(" Ходит 0")
+    
+    x, y = ask_coordinate()
+    
+    if count % 2 == 1:
+        table[x][y] = "X"
+    else:
+        table[x][y] = "0"
+    
+    if check_win():
+        break
+    
+if count == 9:
+    print(" Ничья!")
